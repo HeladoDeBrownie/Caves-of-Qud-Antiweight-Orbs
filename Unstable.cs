5c210d781f45cca516b3728812472cb43dfc207f
helado de brownie <helado.de.brownie@gmail.com>
2019-07-11 19:02:01 -0400

    also destabilize on damage

--- Unstable.cs
+++ Unstable.cs
@@ -4,13 +4,24 @@ using XRL.UI;
 
 namespace XRL.World.Parts {
     [Serializable] public class helado_Unstable : IPart {
+        public const int EXPLODE_ON_HIT_IMPROBABILITY = 10;
+        public const int EXPLODE_EACH_TURN_IMPROBABILITY = 10000;
+
+        public void Destabilize () {
+        Popup.Show ("[destabilize]"); }
+
         public override bool FireEvent (Event e) {
-            if (e.ID == "EndTurn") {
-                if (Stat.Random (1, 10000) <= 1) {
-                    Popup.Show ("[destabilize]"); }
+            if (e.ID == "BeforeApplyDamage") {
+                if (Stat.Random (1, EXPLODE_ON_HIT_IMPROBABILITY) <= 1) {
+                    Destabilize(); }
+                return true; }
+            else if (e.ID == "EndTurn") {
+                if (Stat.Random (1, EXPLODE_EACH_TURN_IMPROBABILITY) <= 1) {
+                     Destabilize(); }
                 return true; }
             else {
                 return false; } }
 
         public override void Register (GameObject go) {
+            go.RegisterPartEvent (this, "BeforeApplyDamage");
             go.RegisterPartEvent (this, "EndTurn"); } } }
