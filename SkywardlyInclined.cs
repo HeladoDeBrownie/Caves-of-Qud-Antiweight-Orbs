1304b01d287d9ce41c98dd38ae30a679847c7cc4
helado de brownie <helado.de.brownie@gmail.com>
2019-07-13 10:33:21 -0400

    unify floating away code

--- SkywardlyInclined.cs
+++ SkywardlyInclined.cs
@@ -4,21 +4,23 @@ using XRL.UI;
 namespace XRL.World.Parts {
     [Serializable] public class helado_SkywardlyInclined : IPart {
 
+    public void FloatAway (GameObject go) {
+        if (go.IsPlayer ()) {
+            go.FireEvent (Event.New ("Die", "Reason", "You floated away and asphixiated in the void of space.", "Accidental", 1)); }
+        else {
+            IPart.AddPlayerMessage (go.The + go.DisplayNameOnly + " floats away.");
+            go.Destroy (); } }
+
     public override bool FireEvent (Event e) {
         if (e.ID == "EndTurn") {
             Physics p = this.ParentObject.pPhysics;
             GameObject haver = p.InInventory ?? p.Equipped;
             if (haver == null) {
                 if (this.ParentObject.IsUnderSky ()) {
-                    IPart.AddPlayerMessage (this.ParentObject.The + this.ParentObject.DisplayNameOnly + " floats away.");
-                    this.ParentObject.Destroy (); } }
+                    FloatAway (this.ParentObject); } }
             else if (haver.IsUnderSky ()) {
                 if (haver.GetPart<Inventory> ().GetWeight () <= -200) {
-                    if (haver.IsPlayer ()) {
-                        haver.FireEvent (Event.New ("Die", "Reason", "You floated away and asphixiated in the void of space.", "Accidental", 1)); }
-                    else {
-                        IPart.AddPlayerMessage (haver.The + haver.DisplayNameOnly + " floats away.");
-                        haver.Destroy (true); } } }
+                    FloatAway (haver); } }
             return true; }
         return false; }
 
