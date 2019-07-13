1a525fe5fc2a6138ac595029bf372710a9f4b2a5
helado de brownie <helado.de.brownie@gmail.com>
2019-07-13 10:15:26 -0400

    proper capitalization

--- SkywardlyInclined.cs
+++ SkywardlyInclined.cs
@@ -10,14 +10,14 @@ namespace XRL.World.Parts {
             GameObject haver = p.InInventory ?? p.Equipped;
             if (haver == null) {
                 if (this.ParentObject.IsUnderSky ()) {
-                    IPart.AddPlayerMessage (this.ParentObject.the + this.ParentObject.DisplayNameOnly + " floats away.");
+                    IPart.AddPlayerMessage (this.ParentObject.The + this.ParentObject.DisplayNameOnly + " floats away.");
                     this.ParentObject.Destroy (); } }
             else if (haver.IsUnderSky ()) {
                 if (haver.GetPart<Inventory> ().GetWeight () <= -200) {
                     if (haver.IsPlayer ()) {
                         haver.FireEvent (Event.New ("Die", "Reason", "You floated away and asphixiated in the void of space.", "Accidental", 1)); }
                     else {
-                        IPart.AddPlayerMessage (haver.the + haver.DisplayNameOnly + " floats away.");
+                        IPart.AddPlayerMessage (haver.The + haver.DisplayNameOnly + " floats away.");
                         haver.Destroy (true); } } }
             return true; }
         return false; }
