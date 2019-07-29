c1f26ecc1a7b0f9a90df7c28dac897d3526bc0e9
helado de brownie <helado.de.brownie@gmail.com>
2019-07-28 22:21:45 -0400

    implement ChargeInvertsWeight

--- ChargeInvertsWeight.cs
+++ ChargeInvertsWeight.cs
@@ -1,4 +1,28 @@
 using System;
 
 namespace XRL.World.Parts {
-    [Serializable] public class helado_ChargeInvertsWeight : IPart {} }
+    [Serializable] public class helado_ChargeInvertsWeight : IPart {
+
+        public bool Active = false;
+
+        public override bool FireEvent (Event E) {
+            switch (E.ID) {
+            case "CellChanged":
+            case "EndTurn":
+                Active = ParentObject.UseCharge (60);
+                break;
+            case "GetWeight":
+                if (Active) {
+                    E.SetParameter ("Weight", -ParentObject.pPhysics._Weight);
+                    return false; }
+                else {
+                    goto default; }
+            default:
+                return base.FireEvent (E); }
+            return true; }
+
+        public override void Register (GameObject Go) {
+            Go.RegisterPartEvent (this, "CellChanged");
+            Go.RegisterPartEvent (this, "EndTurn");
+            Go.RegisterPartEvent (this, "GetWeight");
+            base.Register (Go); } } }
