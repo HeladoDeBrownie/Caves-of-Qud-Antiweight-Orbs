843b2904d017d48d28d49bd7ab19a7249ec8e84f
helado de brownie <helado.de.brownie@gmail.com>
2020-11-04 13:38:09 -0500

    reformat; unbreak explosion code

--- Unstable.cs
+++ Unstable.cs
@@ -1,37 +1,56 @@
 using System;
 using XRL.Rules;
 
-namespace XRL.World.Parts {
-    [Serializable] public class helado_Unstable : IPart {
+namespace XRL.World.Parts
+{
+    [Serializable]
+    public class helado_Unstable : IPart
+    {
         public const int EXPLODE_ON_HIT_IMPROBABILITY = 10;
         public const int EXPLODE_EACH_TURN_IMPROBABILITY = 10000;
 
-        public void Destabilize () {
+        public void Destabilize()
+        {
             GameObject player = this.ParentObject.ThePlayer;
 
-            if (player.OnWorldMap ()) {
-                player.PullDown (); }
+            if (player.OnWorldMap())
+            {
+                player.PullDown();
+            }
 
-            IPart.AddPlayerMessage ("The sphere destabilizes!");
-            Event e = Event.New ("Explode", "Neutron", "1");
-            e.AddParameter ("Force", 3000);
-            e.AddParameter ("Owner", null);
-            e.AddParameter ("BonusDamage", "1d200");
-            this.ParentObject.FireEvent (e);
-            this.ParentObject.Destroy (false); }
+            IPart.AddPlayerMessage("The sphere destabilizes!");
+            ParentObject.Explode(Force: 3000, BonusDamage: "1d200");
+            this.ParentObject.Destroy();
+        }
 
-        public override bool FireEvent (Event e) {
-            if (e.ID == "BeforeApplyDamage") {
-                if (Stat.Random (1, EXPLODE_ON_HIT_IMPROBABILITY) <= 1) {
-                    Destabilize (); }
-                return true; }
-            else if (e.ID == "EndTurn") {
-                if (Stat.Random (1, EXPLODE_EACH_TURN_IMPROBABILITY) <= 1) {
-                     Destabilize (); }
-                return true; }
-            else {
-                return false; } }
+        public override bool FireEvent(Event e)
+        {
+            if (e.ID == "BeforeApplyDamage")
+            {
+                if (Stat.Random(1, EXPLODE_ON_HIT_IMPROBABILITY) <= 1)
+                {
+                    Destabilize();
+                }
+                return true;
+            }
+            else if (e.ID == "EndTurn")
+            {
+                if (Stat.Random(1, EXPLODE_EACH_TURN_IMPROBABILITY) <= 1)
+                {
+                    Destabilize();
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-        public override void Register (GameObject go) {
-            go.RegisterPartEvent (this, "BeforeApplyDamage");
-            go.RegisterPartEvent (this, "EndTurn"); } } }
+        public override void Register(GameObject go)
+        {
+            go.RegisterPartEvent(this, "BeforeApplyDamage");
+            go.RegisterPartEvent(this, "EndTurn");
+        }
+    }
+}
