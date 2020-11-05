96671d817f90b9cfc4b7a1f121f27c2b9943c75b
helado de brownie <helado.de.brownie@gmail.com>
2020-11-04 20:47:18 -0500

    reformat; fix dying from space

--- SkywardlyInclined.cs
+++ SkywardlyInclined.cs
@@ -1,35 +1,63 @@
-using System;
+namespace XRL.World.Parts
+{
+    [System.Serializable]
+    public class helado_SkywardlyInclined : IPart
+    {
 
-namespace XRL.World.Parts {
-    [Serializable] public class helado_SkywardlyInclined : IPart {
+        public void FloatAway(GameObject Go)
+        {
+            if (Go.IsPlayer())
+            {
+                Go.Die(
+                    Killer: (GameObject)null,
+                    Reason: "You floated away and asphixiated in the void of space.",
+                    Accidental: true
+                );
+            }
+            else
+            {
+                IPart.AddPlayerMessage(Go.The + Go.DisplayNameOnly + " floats away!");
+                Go.Destroy();
+            }
+        }
 
-    public void FloatAway (GameObject Go) {
-        if (Go.IsPlayer ()) {
-            Go.FireEvent (Event.New ("Die",
-                "Reason", "You floated away and asphixiated in the void of space.",
-                "Accidental", 1)); }
-        else {
-            IPart.AddPlayerMessage (Go.The + Go.DisplayNameOnly + " floats away.");
-            Go.Destroy (); } }
+        public override bool FireEvent(Event E)
+        {
+            switch (E.ID)
+            {
+                case "EndTurn":
+                    var p = ParentObject.pPhysics;
 
-    public override bool FireEvent (Event E) {
-        switch (E.ID) {
-        case "EndTurn":
-            var p = ParentObject.pPhysics;
+                    if (p.Weight < 0)
+                    {
+                        var haver = p.InInventory ?? p.Equipped;
 
-            if (p.Weight < 0) {
-                var haver = p.InInventory ?? p.Equipped;
+                        if (haver == null)
+                        {
+                            if (ParentObject.IsUnderSky())
+                            {
+                                FloatAway(ParentObject);
+                            }
+                        }
+                        else if (haver.IsUnderSky())
+                        {
+                            if (haver.pPhysics.Weight < 0)
+                            {
+                                FloatAway(haver);
+                            }
+                        }
+                    }
 
-                if (haver == null) {
-                    if (ParentObject.IsUnderSky ()) {
-                        FloatAway (ParentObject); } }
-                else if (haver.IsUnderSky ()) {
-                    if (haver.GetPart<Inventory> ().GetWeight () <= -haver.pPhysics.Weight) {
-                        FloatAway (haver); } } }
-            break;
-        default:
-            return base.FireEvent (E); }
-        return true; }
+                    return true;
 
-    public override void Register (GameObject Go) {
-        Go.RegisterPartEvent (this, "EndTurn"); } } }
+                default:
+                    return base.FireEvent(E);
+            }
+        }
+
+        public override void Register(GameObject Go)
+        {
+            Go.RegisterPartEvent(this, "EndTurn");
+        }
+    }
+}
