b2885d02eb7dcae41362b250fc15ef9a0f9ee164
soupDilemma <helado.de.brownie@gmail.com>
2025-09-08 21:29:51 -0400

    restore overworld booms (almost) to functionality

--- Unstable.cs
+++ Unstable.cs
@@ -2,12 +2,13 @@ using System; // Random
 
 namespace XRL.World.Parts
 {
-    [System.Serializable]
+    [Serializable]
     public class helado_AntiweightOrbs_Unstable : IPart
     {
         public const int EXPLODE_ON_DAMAGE_IMPROBABILITY = 10;
         public const int EXPLODE_EACH_TURN_IMPROBABILITY = 10000;
 
+        [NonSerialized]
         public Random RandomSource;
 
         public void Destabilize()
@@ -33,27 +34,41 @@ namespace XRL.World.Parts
             );
         }
 
+        public override bool WantTurnTick()
+        {
+            return true;
+        }
+
         public override bool WantEvent(int id, int cascade)
         {
             return
                 id == BeforeApplyDamageEvent.ID ||
-                id == EndTurnEvent.ID ||
             base.WantEvent(id, cascade);
+        }
+
+        // BUG: PullDown leaves the player on the world map when called from
+        // TurnTick.
+        public override void TurnTick(long TimeTick, int Amount)
+        {
+            AddPlayerMessage(Amount.ToString());
+            for (int turns = 0; turns < Amount; turns++)
+            {
+                ProcessTurn();
+            }
+            base.TurnTick(TimeTick, Amount);
+        }
+
+        private void ProcessTurn()
+        {
+            if (RandomSource.Next(1, EXPLODE_EACH_TURN_IMPROBABILITY) == 1)
+            {
+                Destabilize();
+            }
         }
 
         public override bool HandleEvent(BeforeApplyDamageEvent @event)
         {
             if (RandomSource.Next(1, EXPLODE_ON_DAMAGE_IMPROBABILITY) == 1)
-            {
-                Destabilize();
-            }
-
-            return true;
-        }
-
-        public override bool HandleEvent(EndTurnEvent @event)
-        {
-            if (RandomSource.Next(1, EXPLODE_EACH_TURN_IMPROBABILITY) == 1)
             {
                 Destabilize();
             }
